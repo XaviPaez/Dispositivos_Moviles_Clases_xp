f90ae004ab49d30f2181d36879b288c6288129bb
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-06 19:36:34 -0500

    Clase del 06/06/2023

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.databinding.PrincipalActivityBinding
+import com.google.android.material.snackbar.Snackbar
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var binding: PrincipalActivityBinding
@@ -30,12 +31,11 @@ class PrincipalActivity : AppCompatActivity() {
 
         //opcion 2
         var name:String=""
-        intent.extras.let {
-            name = it?.getString("var1")!!
-        }
-        Log.d("UCE","Hola ${name}")
+       // intent.extras.let {
+       //     name = it?.getString("var1")!!
+       // }
         binding.txtName.text = "Bienvenido " + name.toString()
-        Log.d("UCE","Entrando a Start")
+
         initClass()
 
     }
@@ -45,6 +45,55 @@ class PrincipalActivity : AppCompatActivity() {
              //el primer parametro es un filtro para que muestre solo eso en la consola al filtar por el termino y el segundo el mensaje
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+
+        }
+
+        binding.bottomNavigation.setOnItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.Inicio -> {
+                    var suma : Int = 0
+                    for(i in listOf<Int>(1,2,3)){
+                        suma = suma+i
+
+                    }
+                    Snackbar.make(binding.txtName,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.Favorito -> {
+                    // Respond to navigation item 2 click
+                    var suma : Int = 0
+                    for(i in listOf<Int>(1,2,30)){
+                        suma = suma+i
+
+                    }
+                    Snackbar.make(binding.txtName,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    // Respond to navigation item 1 click
+                    true
+                }
+
+                R.id.chatgpt -> {
+                    // Respond to navigation item 2 click
+                    var suma : Int = 0
+                    for(i in listOf<Int>(1,10,3)){
+                        suma = suma+i
+
+                    }
+                    Snackbar.make(binding.txtName,
+                        "La suma es ${suma}",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    // Respond to navigation item 1 click
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
