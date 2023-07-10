e603eb0ad7d324cbf2e9a22e44e7457f91449072
Xavier Paez <xavipaez2000@hotmail.com>
2023-07-10 17:26:41 -0500

    Tarea 08 de julio: Buscador

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
@@ -4,26 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
-import com.example.dispositivosmoviles.databinding.FragmentFirst1Binding
 import com.example.dispositivosmoviles.databinding.PrincipalActivityBinding
+import com.example.dispositivosmoviles.ui.fragments.FirstFragment
 import com.example.dispositivosmoviles.ui.fragments.ChatGptFragment
 import com.example.dispositivosmoviles.ui.fragments.FavoriteFragment
-import com.example.dispositivosmoviles.ui.fragments.FirstFragment
 import com.example.dispositivosmoviles.ui.fragments.utilities.FragmentsManager
-import com.google.android.material.snackbar.Snackbar
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var binding: PrincipalActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("UCE","Entrando a Create")
+        Log.d("UCE", "Entrando a Create")
         binding = PrincipalActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -33,57 +28,78 @@ class PrincipalActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        var name:String=""
+        //opcion 1
+//        intent.extras!!.let {
+//            var name = it.getString("var1")
+//        }
 
+        //opcion 2
+        var name: String = ""
+        /*intent.extras.let {
+            name = it?.getString("var1")!!
+        }*/
+        Log.d("UCE", "Hola ${name}")
         binding.txtName.text = "Bienvenido " + name.toString()
-        FragmentsManager().replaceFragmet(
-            supportFragmentManager,
-            binding.frmContainer.id,
-            FavoriteFragment()
-        )
+        Log.d("UCE", "Entrando a Start")
         initClass()
 
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Inicio -> {
+
+//                    val frag = FirstFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
+
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, FirstFragment())
+                    true
+                }
+
+                R.id.Favorito -> {
+//                    val frag = FavoritesFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
+
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, FavoriteFragment()
+                    )
+                    true
+                }
+
+                R.id.chatgpt -> {
+//                    val frag = ChatGptFragment() //crea una instancia del fragment que se quiere agregar
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(binding.frmContainer.id, frag) //se coloca el id del contenedor(framelayout) y se pasa la instancia del fragment que se quiere agregar
+//                    //con .add se inserta otro fragment(mas contenido encima del anterior) encima
+//                    transaction.addToBackStack(null)
+//                    transaction.commit() //guarda los cambios
+
+                    FragmentsManager().replaceFragmet(supportFragmentManager,
+                        binding.frmContainer.id, ChatGptFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 
     private fun initClass() {
         binding.botonRetorno.setOnClickListener {
-             //el primer parametro es un filtro para que muestre solo eso en la consola al filtar por el termino y el segundo el mensaje
+            //el primer parametro es un filtro para que muestre solo eso en la consola al filtar por el termino y el segundo el mensaje
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-
         }
-
-        binding.bottomNavigation.setOnItemReselectedListener { item ->
-            when(item.itemId) {
-                R.id.Inicio -> {
-
-
-
-                    FragmentsManager().replaceFragmet(
-                        supportFragmentManager,
-                        binding.frmContainer.id,
-                        FirstFragment()
-                    )
-
-
-                    true
-                }
-                R.id.Favorito -> {
-                    // Respond to navigation item 2 click
-
-                }
-
-                R.id.chatgpt -> {
-              // o se crea all el fragment o nada
-
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
--- app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/fragments/FavoriteFragment.kt
@@ -5,25 +5,129 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dispositivosmoviles.R
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dispositivosmoviles.data.entities.marvel.MarvelChars
 import com.example.dispositivosmoviles.databinding.FragmentFavoriteBinding
-import com.example.dispositivosmoviles.databinding.FragmentFirst1Binding
+import com.example.dispositivosmoviles.logic.marvelLogic.MarvelLogic
+import com.example.dispositivosmoviles.ui.adapters.MarvelAdapter
+import com.example.dispositivosmoviles.ui.adapters.MarvelAdapterBuscador
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FavoriteFragment : Fragment() {
     private lateinit var binding: FragmentFavoriteBinding
+    private lateinit var gManager: GridLayoutManager
+    private lateinit var lmanager: LinearLayoutManager
+
+    private var rvAdapter: MarvelAdapterBuscador = MarvelAdapterBuscador()
+
+    private var marvelCharsItems: MutableList<MarvelChars> = mutableListOf<MarvelChars>()
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
 
-        binding = FragmentFavoriteBinding.inflate(
-            layoutInflater,
-            container,
+        lmanager = LinearLayoutManager(
+            requireActivity(),
+            LinearLayoutManager.VERTICAL,
             false
         )
-        // Inflate the layout for this fragment
+
+
+        gManager = GridLayoutManager(requireActivity(), 2)
+
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        chargeDataRV()
+//        binding.rvSwipe.setOnRefreshListener {
+//            chargeDataRV()
+//            binding.rvSwipe.isRefreshing = false
+//        }
+//        //Para cargar mas contenido
+//        binding.rvMarvelChars.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                super.onScrolled(
+//                    recyclerView,
+//                    dx,
+//                    dy
+//                ) //dy es para el scroll de abajo y dx es de izquierda a derech para buscar elementos
+//
+//                if (dy > 0) {
+//                    val v = lmanager.childCount  //cuantos elementos han pasado
+//                    val p = lmanager.findFirstVisibleItemPosition() //posicion actual
+//                    val t = lmanager.itemCount //cuantos tengo en total
+//
+//                    //necesitamos comprobar si el total es mayor igual que los elementos que han pasado entonces ncesitamos actualizar ya que estamos al final de la lista
+//                    if ((v + p) >= t) {
+//                        chargeDataRV()
+//                        lifecycleScope.launch((Dispatchers.IO)) {
+//                            val newItems = MarvelLogic().getAllMarvelChars(0, 99)
+//                            withContext(Dispatchers.Main) {
+//                                rvAdapter.updateListItems(newItems)
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//
+//        })
+
+        binding.txtFilter.addTextChangedListener { filteredText ->
+            if (filteredText.toString().isNullOrEmpty()) {
+                chargeDataRVVacios()
+            } else {
+
+                val newItems = marvelCharsItems.filter { items ->
+                    items.name.lowercase().contains(filteredText.toString().lowercase())
+                }
+                rvAdapter.replaceListItems(newItems)
+                binding.rvMarvelChars.apply {
+                    this.adapter = rvAdapter
+                    this.layoutManager = gManager
+                }
+            }
+        }
+
+    }
+
+
+    fun chargeDataRV() {
+
+
+        lifecycleScope.launch(Dispatchers.Main) {
+
+            marvelCharsItems = withContext(Dispatchers.IO) {
+
+
+                return@withContext (MarvelLogic().getAllMarvelChars(0, 99))
+            }
+
+
+        }
+
+
+    }
+
+
+    fun chargeDataRVVacios() {
+        rvAdapter.items = listOf<MarvelChars>()
+
+        binding.rvMarvelChars.apply {
+            this.adapter = rvAdapter
+        }
     }
 
 
